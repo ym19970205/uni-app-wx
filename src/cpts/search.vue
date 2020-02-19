236cf4120bbbb8574aaa413a3b2761d1e408cb3e
Your Name <you@example.com>
2020-02-19 22:32:07 +0800

    分类--公共搜索组件完成

---
+++ src/cpts/search.vue
@@ -0,0 +1,40 @@
+<template>
+  	<!-- 搜索框 -->
+		<view class="search">
+			<view class="search_box">
+				<!-- 搜索栏 -->
+			<input type="text">
+				<!--取消按钮  -->
+			</view> 
+		</view>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            
+        }
+    }
+
+}
+</script>
+
+<style lang="less">
+ //搜索区
+		.search{
+			background-color: #ff2d4a;
+			box-sizing: border-box;
+			padding: 20rpx 16rpx;
+			.search_box{
+				//搜索区
+				input{
+				height: 60rpx;
+				width: 100%;
+				background-color: #fff;
+				border-radius: 5rpx;
+			}
+			}
+		
+		}
+</style>
---
+++ src/pages/category/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <view class="category">
+      <!-- 搜索组件 -->
+    <search />
+  </view>
+
+</template>
+
+<script>
+	//1.导入搜索组件
+import {search} from "@/cpts/search.vue"
+export default {
+    data(){
+
+    },
+    //2.注册搜索组件
+	components:{
+		search
+	}
+}
+</script>
+
+<style lang="less">
+
+</style>
